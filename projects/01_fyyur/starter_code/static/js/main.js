454b631364f6492913788d368f53d4722d3af184
Ikechukwu Iwuh <59923488+Ikman94@users.noreply.github.com>
2021-11-01 13:56:10 -0400

    Create & Edit Controllers Commit

---
+++ projects/01_fyyur/starter_code/static/js/main.js
@@ -0,0 +1,20 @@
+// let artist_name = document.getElementById('artist-name-input');
+// let artist_city = document.getElementById('artist-city-input');
+// let artist_name = document.getElementById('artist-name-input');
+// let artist_name = document.getElementById('artist-name-input');
+// let artist_name = document.getElementById('artist-name-input');
+// let artist_name = document.getElementById('artist-name-input');
+// let artist_name = document.getElementById('artist-name-input');
+
+
+// document.getElementById('artist-form').onsubmit = function (e) {
+//     e.preventDefault();
+    
+//     fetch('create/artist',{
+//         method: 'POST',
+//         body: JSON.stringify({
+//             'name': document
+//         })
+//     })
+
+// }
