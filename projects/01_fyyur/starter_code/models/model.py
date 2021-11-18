2eeded94e38a72e6e423c0621b4ff0bbec963b69
Ikechukwu Iwuh <59923488+Ikman94@users.noreply.github.com>
2021-11-18 11:06:01 -0500

    commit

--- projects/01_fyyur/starter_code/models/model.py
+++ projects/01_fyyur/starter_code/models/model.py
@@ -21,7 +21,12 @@ class Venue(db.Model):
     website_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean())
     seeking_description = db.Column(db.String(500))
-    venue_show = db.relationship('Shows', backref='venue', lazy=True)
+    shows = db.relationship(
+        'Show', 
+        backref='venue', 
+        lazy='joined', 
+        cascade="all, delete"
+    )
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -40,12 +45,17 @@ class Artist(db.Model):
     website_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean())
     seeking_description = db.Column(db.String(500))
-    artist_show = db.relationship('Shows', backref='artist', lazy=True)
+    shows = db.relationship(
+        'Show', 
+        backref='artist', 
+        lazy='joined', 
+        cascade="all, delete"
+    )
 
-class Shows(db.Model):
-    __tablename__ ='Shows'
+class Show(db.Model):
+    __tablename__ ='Show'
 
     id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id', ondelete='CASCADE'), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id', ondelete='CASCADE'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False)
